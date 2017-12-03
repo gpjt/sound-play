72318068c6e5bd9fc32b82e59c1734aebc5badc9
Giles Thomas <giles@giles.net>
2017-12-03 23:32:41 +0000

    Refactored square wave generator

--- square.js
+++ square.js
@@ -1,66 +1,20 @@
-const Readable = require('stream').Readable;
-const bufferAlloc = require('buffer-alloc');
 const Speaker = require('speaker');
+const Oscillator = require('./oscillator').Oscillator;
 
-// the frequency to play
-const freq = parseFloat(process.argv[2], 10) || 440.0; // Concert A, default tone
+class SquareWaveOscillator extends Oscillator {
 
-// seconds worth of audio data to generate before emitting "end"
-const duration = parseFloat(process.argv[3], 10) || 2.0;
-
-console.log('generating a %dhz square wave for %d seconds', freq, duration);
-
-class SquareWaveGenerator extends Readable {
-
-    constructor(bitDepth, channels, sampleRate) {
-        super();
-        this.bitDepth = bitDepth;
-        this.channels = channels;
-        this.sampleRate = sampleRate;
-        this.samplesGenerated = 0;
-    }
-
-
-    _read(n) {
-        const sampleSize = this.bitDepth / 8;
-        const blockAlign = sampleSize * this.channels;
-        const numSamples = n / blockAlign | 0;
-        const buf = bufferAlloc(numSamples * blockAlign);
-        const amplitude = 32760; // Max amplitude for 16-bit audio
-
-        // the "angle" used in the function, adjusted for the number of
-        // channels and sample rate. This value is like the period of the wave.
-        const t = freq / this.sampleRate;
-
-        for (let i = 0; i < numSamples; i++) {
-            // fill with a simple square wave at max amplitude
-            for (let channel = 0; channel < this.channels; channel++) {
-                const s = this.samplesGenerated + i;
-                let val;
-                if (((t * s) % 1) < 0.5) {
-                    val = amplitude;
-                } else {
-                    val = -amplitude;
-                }
-                const offset = (i * sampleSize * this.channels) + (channel * sampleSize);
-                buf[`writeInt${this.bitDepth}LE`](val, offset);
-            }
-        }
-
-        this.push(buf);
-
-        this.samplesGenerated += numSamples;
-        if (this.samplesGenerated >= this.sampleRate * duration) {
-            // after generating "duration" second of audio, emit "end"
-            this.push(null);
+    generateAmplitude(timeOffsetInCycles) {
+        if ((timeOffsetInCycles % 1) < 0.5) {
+            return 1;
+        } else {
+            return -1;
         }
     }
-
 
 }
 
-// A SquareWaveGenerator readable stream
-const square = new SquareWaveGenerator(16, 2, 44100);
-
-// create a SquareWaveGenerator instance and pipe it to the speaker
+const freq = parseFloat(process.argv[2], 10) || 440.0; // Concert A, default tone
+const duration = parseFloat(process.argv[3], 10) || 2.0;
+console.log('generating a %dhz square wave for %d seconds', freq, duration);
+const square = new SquareWaveOscillator(16, 2, 44100, freq, duration);
 square.pipe(new Speaker());
