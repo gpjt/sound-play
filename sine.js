8609b24cb527edcc206dd7014345e52b5f26ce2c
Giles Thomas <giles@giles.net>
2017-12-03 23:21:52 +0000

    Factored out a general oscillator class.

--- sine.js
+++ sine.js
@@ -1,6 +1,5 @@
-const Readable = require('stream').Readable;
-const bufferAlloc = require('buffer-alloc');
 const Speaker = require('speaker');
+const Oscillator = require('./oscillator').Oscillator;
 
 // the frequency to play
 const freq = parseFloat(process.argv[2], 10) || 440.0; // Concert A, default tone
@@ -10,52 +9,16 @@ const duration = parseFloat(process.argv[3], 10) || 2.0;
 
 console.log('generating a %dhz sine wave for %d seconds', freq, duration);
 
-class SineWaveGenerator extends Readable {
+class SineWaveGenerator extends Oscillator {
 
-    constructor(bitDepth, channels, sampleRate) {
-        super();
-        this.bitDepth = bitDepth;
-        this.channels = channels;
-        this.sampleRate = sampleRate;
-        this.samplesGenerated = 0;
+    generateAmplitude(timeOffsetInCycles) {
+        return Math.sin(Math.PI * 2 * timeOffsetInCycles);
     }
-
-
-    _read(n) {
-        const sampleSize = this.bitDepth / 8;
-        const blockAlign = sampleSize * this.channels;
-        const numSamples = n / blockAlign | 0;
-        const buf = bufferAlloc(numSamples * blockAlign);
-        const amplitude = 32760; // Max amplitude for 16-bit audio
-
-        // the "angle" used in the function, adjusted for the number of
-        // channels and sample rate. This value is like the period of the wave.
-        const t = (Math.PI * 2 * freq) / this.sampleRate;
-
-        for (let i = 0; i < numSamples; i++) {
-            // fill with a simple sine wave at max amplitude
-            for (let channel = 0; channel < this.channels; channel++) {
-                const s = this.samplesGenerated + i;
-                const val = Math.round(amplitude * Math.sin(t * s)); // sine wave
-                const offset = (i * sampleSize * this.channels) + (channel * sampleSize);
-                buf[`writeInt${this.bitDepth}LE`](val, offset);
-            }
-        }
-
-        this.push(buf);
-
-        this.samplesGenerated += numSamples;
-        if (this.samplesGenerated >= this.sampleRate * duration) {
-            // after generating "duration" second of audio, emit "end"
-            this.push(null);
-        }
-    }
-
 
 }
 
 // A SineWaveGenerator readable stream
-const sine = new SineWaveGenerator(16, 2, 44100);
+const sine = new SineWaveGenerator(16, 2, 44100, freq, duration);
 
 // create a SineWaveGenerator instance and pipe it to the speaker
 sine.pipe(new Speaker());
