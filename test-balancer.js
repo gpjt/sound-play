68ef056006a18c2ed5d022dcf06d0d871b542f8e
Giles Thomas <giles@giles.net>
2017-12-04 00:20:12 +0000

    Split balancer test code out of the module too

---
+++ test-balancer.js
@@ -0,0 +1,10 @@
+const stream = require('stream');
+const Speaker = require('speaker');
+const SineWaveOscillator = require('./oscillator').SineWaveOscillator;
+const Balancer = require('./balancer').Balancer;
+
+const freq = parseFloat(process.argv[2], 10) || 440.0; // Concert A, default tone
+const duration = parseFloat(process.argv[3], 10) || 2.0;
+console.log('generating a %dhz sine wave for %d seconds', freq, duration);
+const sine = new SineWaveOscillator(16, 2, 44100, freq, duration);
+sine.pipe(new Balancer(1)).pipe(new Speaker());
