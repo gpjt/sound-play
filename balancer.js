718a6ecc912ce47aa1ebfc24de6d0e00bb986998
Giles Thomas <giles@giles.net>
2017-12-04 01:01:15 +0000

    Implemented balancer

--- balancer.js
+++ balancer.js
@@ -1,14 +1,32 @@
+const assert = require('assert');
+const bufferAlloc = require('buffer-alloc');
 const stream = require('stream');
 
 class Balancer extends stream.Transform {
 
-    constructor(position) {
+    constructor(bitDepth, channels, position) {
         super();
+        assert.ok(channels === 2, "Can only balance with two channels!")
+        this.bitDepth = bitDepth;
+        this.channels = channels;
         this.position = position;
+        this.bytesPerSamplePerChannel = this.bitDepth / 8;
+        this.bytesPerSample = this.bytesPerSamplePerChannel * this.channels;
     }
 
     _transform(chunk, encoding, callback) {
-        this.push(chunk);
+        const newChunk = bufferAlloc(chunk.length);
+
+        for (let offset=0; offset < chunk.length; offset+=this.bytesPerSample) {
+            const sampleLeft = chunk[`readInt${this.bitDepth}LE`](offset);
+            const sampleRight = chunk[`readInt${this.bitDepth}LE`](offset + this.bytesPerSamplePerChannel);
+            const newSampleLeft = sampleLeft * (1 - this.position);
+            const newSampleRight = sampleRight * this.position;
+            newChunk[`writeInt${this.bitDepth}LE`](newSampleLeft, offset);
+            newChunk[`writeInt${this.bitDepth}LE`](newSampleRight, offset + this.bytesPerSamplePerChannel);
+        }
+
+        this.push(newChunk);
         callback();
     }
 
--- test-balancer.js
+++ test-balancer.js
@@ -6,4 +6,5 @@ const freq = parseFloat(process.argv[2], 10) || 440.0; // Concert A, default ton
 const duration = parseFloat(process.argv[3], 10) || 2.0;
 console.log('generating a %dhz sine wave for %d seconds', freq, duration);
 const sine = new SineWaveOscillator(16, 2, 44100, freq, duration);
-sine.pipe(new Balancer(1)).pipe(new Speaker());
+const balancer = new Balancer(16, 2, 0)
+sine.pipe(balancer).pipe(new Speaker());
